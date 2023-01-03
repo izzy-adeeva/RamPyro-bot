04644b58c1870e999eee1e4807ab806d3cf28a91
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-03 16:03:05 +0700

    Update broadcast.py

--- rams/modules/broadcast.py
+++ rams/modules/broadcast.py
@@ -26,7 +26,7 @@ from .help import add_command_help
 
 while 0 < 6:
     _GCAST_BLACKLIST = get(
-        "https://raw.githubusercontent.com/ramadhani892/Ramblack/master/blacklistgcast.json"
+        "https://raw.githubusercontent.com/vckyou/Reforestation/master/blacklistgcast.json"
     )
     if _GCAST_BLACKLIST.status_code != 200:
         if 0 != 5:

49a306629b699990ad84789d87481568b70ad143
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-10 02:42:35 +0700

    Update __init__.py

--- rams/__init__.py
+++ rams/__init__.py
@@ -8,6 +8,7 @@ from logging.handlers import RotatingFileHandler
 from typing import Any, Dict
 
 from aiohttp import ClientSession
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gpytranslate import Translator
 from pyrogram import Client
 from pytgcalls import GroupCallFactory
@@ -90,6 +91,7 @@ aiosession = ClientSession()
 
 CMD_HELP = {}
 
+scheduler = AsyncIOScheduler()
 
 StartTime = time.time()
 

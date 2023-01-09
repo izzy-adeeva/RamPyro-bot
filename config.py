371245657608221e633acfc37984273f1a181a96
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-09 22:04:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ load_dotenv("config.env")
 ALIVE_EMOJI = getenv("ALIVE_EMOJI", "👑")
 ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/eda4816a97d4e74445fc0.jpg")
 ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey bro, I am rams.")
-API_HASH = getenv("API_HASH")
-API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "34efb38c74d5e6b25d1bb6234396a8af")
+API_ID = (getenv("API_ID", "23129036")
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
     BLACKLIST_CHAT = [-1001692751821]
